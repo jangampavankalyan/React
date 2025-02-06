f6e8870357e0fcd81075d4fc96d44cdc0a604167
Pavan Kalyan <jangampavankalyan03@gmail.com>
2025-02-06 10:44:52 +0530

    Enhancement of First Chapter Project

---
+++ 01-starting-project/src/components/CoreConcepts/CoreConcepts.jsx
@@ -0,0 +1,15 @@
+import { CORE_CONCEPTS } from "../../data";
+import { CoreConcept } from "../../components/CoreConcept/CoreConcept";
+
+export default function CoreConcepts() {
+  return (
+    <section id="core-concepts">
+      <h2>Core Concepts</h2>
+      <ul>
+        {CORE_CONCEPTS.map((conceptItem) => (
+          <CoreConcept key={conceptItem.title} {...conceptItem} />
+        ))}
+      </ul>
+    </section>
+  );
+}
