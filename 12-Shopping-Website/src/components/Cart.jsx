d97bf3da3a7953bae044d10bfe7757078a56f72e
Pavan Kalyan <jangampavankalyan03@gmail.com>
2025-04-09 14:33:42 +0530

    Progress on Shopping website - 1

--- 12-Shopping-Website/src/components/Cart.jsx
+++ 12-Shopping-Website/src/components/Cart.jsx
@@ -1,4 +1,10 @@
-export default function Cart({ items, onUpdateItemQuantity }) {
+import { useContext } from "react";
+
+import { CartContent } from "../store/shopping-cart-context";
+
+export default function Cart({ onUpdateItemQuantity }) {
+  const { items } = useContext(CartContent);
+
   const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0
